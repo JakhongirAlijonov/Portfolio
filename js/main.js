a3247861a6dc9487dd093b8a96046c4488afbc5e
jakhongiralijonov <jakhongirweb@gmail.com>
2022-01-08 01:08:02 +0500

    responsive

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// const burger = document.querySelector('.burger')
+// const brackdrop = document.querySelector('.brackdrop')
+
+// let mobile = document.querySelector('.mobila-navs')
+// burger.addEventListener('click', () => {
+//     mobile.classList.remove('none')
+//     document.body.classList.add('brackdrop')
+
+// })
+
+// brackdrop.addEventListener('click', () => {
+//     brackdrop.style.display = 'none'
+//     mobile.classList.add('none')
+//     console.log(brackdrop);
+// })
+
+// console.log(brackdrop);
